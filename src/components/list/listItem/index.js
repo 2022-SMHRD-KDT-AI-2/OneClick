a36e71ee8c02d55f0be3ecfab029fbc413f8349b
94jinhyeok <94jinhyeok@gmail.com>
2022-03-09 23:54:56 +0900

    list commit

--- src/components/list/listItem/index.js
+++ src/components/list/listItem/index.js
@@ -15,10 +15,13 @@ function ListItem() {
     {/* 좌측 라인 스타일(가게 이미지 들어갈 공간) */}
     <div className="img" style={{
         display: "flex",
-        borderTop: "5px solid gray",
-        borderLeft: "5px solid gray"
+        border: "5px solid gray",
+       
       }}>
-    <image src= 'https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTExMDFfMzYg%2FMDAxNjM1Njk3NDIwNTU5.UHdC_gYLLoABUPwHWmoiS9Grn1usNX_L0cdNc-M_Bkcg.qv9p5ND74MgZtGm3nuWqjrSUKeMSrcC4KSIayO_OZxEg.JPEG.kgpdnjs1998%2FIMG_4506.JPG' width='50' height='50'></image>
+    <img src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=https%3A%2F%2Fmyplace-phinf.pstatic.net%2F20211122_246%2F1637574504180hm4HF_JPEG%2Fupload_3738f6c056530c843842efd81c0618b3.jpg"style={{
+      width: "150px",
+      height: "150px"
+    }}></img>
     <div class="shop">
       <div class="content" style={{
         fontSize: "15px",
@@ -30,28 +33,15 @@ function ListItem() {
           <p className="division" style={{
             color: "gray"
           }}>분류 | 디저트 / 카페</p>
-          <p className="shop-dwelling">주소 | 광주 동구 백서로 219 광주 동구 동명동 140-13 61466</p>
+         <p className="complexity" style={{float:"left"}}>복잡도 | </p><div className="table"></div>
+                  </div>
+         
           
 
     </div>
     </div>
-          {/* 오른쪽 상단 배너에 가게 혼잡도 컬러 넣기 */}
-          
+    
 
-        
-        <div className="box2" style={{
-          flexDirection: "columns",
-          color: "black",
-          borderRight: "5px solid gray"
-        }}>
-        <div style={{
-          textAlign: "right"
-        }}>
-           </div>
-            {/* 컬러(스타일) 클래스 */}
-            <div className="table"></div>
-        </div>
-      </div>
   </ListItemContainer>;
 }
 
